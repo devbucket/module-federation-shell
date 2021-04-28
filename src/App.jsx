fe8d7a9cab32ac5237b78641cb3a1fece502cee8
devbucket <flomueller1981@gmail.com>
2021-04-28 12:09:11 +0200

    Use a reducer from a sub-app

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
 import React, { memo } from 'react';
 import { Container } from 'react-bootstrap';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { profileReducer } from 'profile/reducers';
 import { NavBar } from './components/NavBar';
 import { HomeRoute, ProfileRoute } from './routes';
+
+console.warn(profileReducer);
 
 export const App = memo(
   function App() {
